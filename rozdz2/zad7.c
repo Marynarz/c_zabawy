8474969bcc58d7af4089355dabcc7a9b491580c0
Wojciech Niedzielski <wojciech.niedzielski@km.kongsberg.com>
2023-07-04 09:34:55 +0200

    zad7

---
+++ rozdz2/zad7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const float inch_to_cal = 2.54;
+    float scanned_height;
+
+    printf("Podaj wzrost w cm: ");
+    scanf("%f", &scanned_height);
+
+    printf("Wzrost %fcm to %finch\n", scanned_height,scanned_height / inch_to_cal);
+
+    return 0;
+}
