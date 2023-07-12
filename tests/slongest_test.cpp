e676143c8da5c0f26b5c83384c47d884bb031c80
Wojciech Niedzielski <wojciech.niedzielski@km.kongsberg.com>
2023-07-12 11:08:06 +0200

    add cmakefile and test config

---
+++ tests/slongest_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "../sem/longest.c"
+
+TEST(LeetCodeSubsests, EmptyVectorTest)
+{
+    ASSERT_TRUE(1);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
