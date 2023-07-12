fa2c3248bcf8d290d24f51c352ff579bf610e485
Wojciech Niedzielski <wojciech.niedzielski@km.kongsberg.com>
2023-07-12 12:18:13 +0200

    all tests passed

--- sem/longest.c
+++ sem/longest.c
@@ -1,10 +1,37 @@
+#include <stdio.h>
+
 int longestSubarray(int *nums, int numsSize)
 {
     if (numsSize == 0 || numsSize == 1)
         return 0;
 
+    int prev_long = 0;
+    int act_long = 0;
+    int longest = 0;
+    int act_distance = 0;
+
     for (int i = 0; i < numsSize; i++)
     {
+        printf("i: %d, val:%d, act_long: %d, prev_long: %d\n", i, nums[i], act_long, prev_long);
+        if (nums[i] == 0)
+        {
+            act_distance = prev_long + act_long;
+            if (act_distance > longest)
+                longest = act_distance;
+
+            prev_long = act_long;
+            act_long = 0;
+        }
+        else
+            ++act_long;
     }
-    return 0;
+
+    act_distance = prev_long + act_long;
+    if (act_distance > longest)
+        longest = act_distance;
+
+    if (longest == numsSize)
+        --longest;
+
+    return longest;
 }
--- tests/slongest_test.cpp
+++ tests/slongest_test.cpp
@@ -4,32 +4,59 @@
 
 TEST(LeetCodeSubsests, EmptyList)
 {
-    int * test_array = NULL;
+    int *test_array = NULL;
 
-    int result = longestSubarray(test_array ,0);
+    int result = longestSubarray(test_array, 0);
 
     ASSERT_EQ(result, 0);
 }
 
 TEST(LeetCodeSubsets, OneElementList)
 {
-    int * test_array = new int[1];
+    int *test_array = new int[1];
     test_array[0] = 0;
 
     int result = longestSubarray(test_array, 1);
 
-    delete [] test_array;
+    delete[] test_array;
 
     ASSERT_EQ(result, 0);
 }
 
-TEST(LeetCodeSubsets, TwoElementsList)
+TEST(LeetCodeSubsets, TwoElementsListLastZero)
 {
-    std::array<int, 2> test_array{1,0};
+    std::array<int, 2> test_array{1, 0};
 
     int result = longestSubarray(test_array.data(), test_array.size());
 
     ASSERT_EQ(result, 1);
+}
+
+TEST(LeetCodeSubsets, TwoElementsListFirstZero)
+{
+    std::array<int, 2> test_array{0, 1};
+
+    int result = longestSubarray(test_array.data(), test_array.size());
+
+    ASSERT_EQ(result, 1);
+}
+
+TEST(LeetCodeSubsets, TwoElementsListOnlyOnes)
+{
+    std::array<int, 2> test_array{1, 1};
+
+    int result = longestSubarray(test_array.data(), test_array.size());
+
+    ASSERT_EQ(result, 1);
+}
+
+TEST(LeetCodeSubsets, TwoElementsListOnlyZeroes)
+{
+    std::array<int, 2> test_array{0, 0};
+
+    int result = longestSubarray(test_array.data(), test_array.size());
+
+    ASSERT_EQ(result, 0);
 }
 
 int main(int argc, char **argv)
